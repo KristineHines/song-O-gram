33df2c94a51f77456d5d4d09e9b3acf9768b8b91
Kristine Hines <hines.kristine@gmail.com>
2013-07-10 16:17:05 -0700

    recipe migration)

--- db/migrate/20130705211756_create_recipes.rb
+++ db/migrate/20130705211756_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
       t.string :title, null: false
-      t.reference :user, null: false
+      t.references :user, null: false
       t.timestamps
     end
   end
