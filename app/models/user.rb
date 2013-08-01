638f09247c34b58a5f02a1ab6819ab1384db6be1
Kristine Hines <Hines.kristine@gmail.com>
2013-07-31 17:21:14 -0700

    deleted all attr-accessible references, commented out whitelist_attribute option

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :first_name, :last_name, :email, :username, :password, :password_confirmation, :admin
   has_many :recipes
   validates_confirmation_of :password
 
