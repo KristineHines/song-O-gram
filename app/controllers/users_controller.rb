7682fc9cd6b2f24060009f236e4f1c4ac748628b
Kristine Hines <hines.kristine@gmail.com>
2013-08-14 16:58:40 -0700

    downloaded devise views for customizations, user can signin/out and hold a session, edit and cancel the account. added ssl-not yet working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,39 +6,4 @@ class UsersController < ApplicationController
 
   def show
   end
-  
-  def new
-    @user = User.new
-  end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to @user
-  #   else
-  #     @errors = @user.errors.full_messages
-  #     render :new
-  #   end
-  # end
-
-  # def edit
-  #   @user = User.find(params[:id])
-  # end
-
-  # def update
-  #   @user = User.find(params[:id])
-  #   @user.update_attributes!(user_params)
-  #   redirect_to @user
-  # end
-
-  # def destroy
-  #   @user = User.find(params[:id]).destroy
-  #   redirect_to :root
-  # end
-
-  private
-
-  def user_params
-    params.require(:user).permit!
-  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130814214129) do
+ActiveRecord::Schema.define(version: 20130814225602) do
+
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "amounts", force: true do |t|
     t.integer  "number",     null: false
@@ -67,7 +85,6 @@ ActiveRecord::Schema.define(version: 20130814214129) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
